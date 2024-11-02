f7dfcfd339adcbd54bc36404c5ae6d0b4eb072d4
psnovikova <nps20052000@mail.ru>
2024-11-02 03:17:22 +0300

    feat: разделение компонентов создания и редактирования

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,56 @@
 <template>
-  <CreatePlayer @players-list="createdPlayers" />
-  <EditPlayers :playersList="playersList" />
+  <div>
+    <div>
+      <button
+        class="tab-btn"
+        :class="{ active: currentView === 'create' }"
+        @click="currentView = 'create'"
+      >
+        Создание
+      </button>
+      <button
+        class="tab-btn"
+        :class="{ active: currentView === 'edit' }"
+        @click="currentView = 'edit'"
+      >
+        Редактирование
+      </button>
+    </div>
+
+    <div v-if="currentView === 'create'">
+      <CreatePlayer @players-list="createdPlayers" />
+    </div>
+    <div v-else>
+      <EditPlayers :playersList="playersList" />
+    </div>
+  </div>
 </template>
 
 <script>
-import CreatePlayer from './components/CreatePlayer.vue'
-import EditPlayers from './components/EditPlayers.vue'
+import CreatePlayer from "./components/CreatePlayer.vue";
+import EditPlayers from "./components/EditPlayers.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
     CreatePlayer,
-    EditPlayers
+    EditPlayers,
   },
   data() {
     return {
-      playersList: []
-    }
+      playersList: [],
+      currentView: "create",
+    };
   },
 
-  created() {
-    
-  },
+  created() {},
 
   methods: {
     createdPlayers(list) {
       this.playersList = list;
-    }
+    },
   },
-}
+};
 </script>
 
 <style>
@@ -40,5 +62,17 @@ export default {
   color: #2c3e50;
   margin: 60px auto;
   width: 600px;
+}
+
+.tab-btn {
+  padding: 10px 20px;
+  margin: 0 10px;
+  cursor: pointer;
+  font-size: 16px;
+}
+
+.tab-btn.active {
+  background-color: #00bd69;
+  color: #fff;
 }
 </style>
